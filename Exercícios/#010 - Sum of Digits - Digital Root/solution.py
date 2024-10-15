f2103b8dbb9a1311030f3b355f8e77547e524b68
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-10-15 13:15:16 -0300

    solução

--- Exercícios/#010 - Sum of Digits - Digital Root/solution.py
+++ Exercícios/#010 - Sum of Digits - Digital Root/solution.py
@@ -8,6 +8,17 @@
 # 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
 # 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
 
-def digital_root(n):
-    # ...
-    pass
+# 1 - Verificar se possui mais de um algarismo
+    # Se sim, seguir o fluxo abaixo:
+# 2 - Soma os algarismos
+# Repete até possuir apenas um algarismo
+
+def digital_root(n):  
+    while len(str(n)) > 1: # Verificar se possui mais de um algarismo
+        soma = 0
+        for algarismo in str(n):
+            soma += int(algarismo)
+        n = soma
+    return n
+
+print(digital_root(132189))
