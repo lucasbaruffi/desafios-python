7b9808846d4fb37d05c54c38f0c20ce02271ddb7
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-10-16 08:28:35 -0300

    solução

--- Exercícios/#011 - Sum of Digits - Roman Numerals Encoder/solution.py
+++ Exercícios/#011 - Sum of Digits - Roman Numerals Encoder/solution.py
@@ -23,6 +23,53 @@
 # D          500
 # M          1,000
 
+# 1 - Verificar do maior ao menor
+
+
+# 1 - I
+# 2 - II
+# 3 - III
+# 4 - IV
+# 5 - V
+# 6 - VI
+# 7 - VII
+# 8 - VIII
+# 9 - IX
+
+
+
 def solution(n):
-    # TODO convert int to roman string
-    return
+    
+    romanoFinal = ""
+
+    # Se tiver milhar
+    if n // 1000:
+        milharDecimal = n // 1000
+        n = n % 1000
+        milharRomano = ("M" * milharDecimal) # Como o desafio vai até 3999, só isso é necessário
+        romanoFinal = milharRomano
+
+    # Se tiver centena
+    if n // 100:
+        centenaDecimal = n // 100 # Pega a Centena, capturando a divisão inteira
+        n = n % 100 # Define o restante como o resto da divisão, na variável principal para chegar apenas isso no próximo
+        listaCentenaRomano = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"] # Lista com as possibilidades
+        centenaRomano = listaCentenaRomano[centenaDecimal] # A Localização do decimal dentro da lista
+        romanoFinal += centenaRomano # Adiciona ao main
+
+    # Se tiver decimal
+    if n // 10:
+        dezenaDecimal = n // 10
+        n = n % 10
+        listaDecimalRomano = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        decimalRomano = listaDecimalRomano[dezenaDecimal]
+        romanoFinal += decimalRomano
+
+    # Se tiver Unidade
+    unidadeDecimal = n
+    listaUnidadeRomano = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    unidadeRomano = listaUnidadeRomano[unidadeDecimal]
+    romanoFinal += unidadeRomano
+    return romanoFinal
+
+print(solution(1666))
