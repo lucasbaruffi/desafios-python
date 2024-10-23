bc1cf9f0d358cbbd0ffc81523299c999575b9f16
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-10-23 10:22:08 -0300

    solução

--- Exercícios/#016 - Human Readable Time/solution.py
+++ Exercícios/#016 - Human Readable Time/solution.py
@@ -6,4 +6,14 @@
 # he maximum time never exceeds 359999 (99:59:59)
 
 def make_readable(seconds):
-    pass
+    horas = 00
+    segundos = seconds % 60
+    minutos = seconds // 60
+    if minutos > 59:
+        horas = minutos // 60
+        minutos = minutos % 60        
+    return f"{str(horas).zfill(2)}:{str(minutos).zfill(2)}:{str(segundos).zfill(2)}"
+
+
+
+print(make_readable(359999))
