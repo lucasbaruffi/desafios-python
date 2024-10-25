90c5a7298045585e5ca28c4beb4fea8e6080a8cd
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-10-25 14:49:54 -0300

    éh
    falta converter de decimal pra python, já fiz isso algum dia

--- Exercícios/#020 - int32 to IPv4/solution.py
+++ Exercícios/#020 - int32 to IPv4/solution.py
@@ -18,5 +18,33 @@
 # 0          ==> "0.0.0.0"
 
 def int32_to_ip(int32):
-    # your code here
-    return
+    binario = bin(int32)
+    print(binario)
+#    binario = str(binario).zfill(32)
+#    print(binario)
+#    print(divideint32(binario))
+
+# Função para dividir um número de 32 dígitos em 4 de 8
+def divideint32(int32):
+    n1 = int32[0],int32[1],int32[2],int32[3],int32[4],int32[5],int32[6],int32[7]
+    n2 = int32[8],int32[9],int32[10],int32[11],int32[12],int32[13],int32[14],int32[15]
+    n3 = int32[16],int32[17],int32[18],int32[19],int32[20],int32[21],int32[22],int32[23]
+    n4 = int32[24],int32[25],int32[26],int32[27],int32[28],int32[29],int32[30],int32[31]
+
+    n1 = "".join(n1)
+    n2 = "".join(n2)
+    n3 = "".join(n3)
+    n4 = "".join(n4)
+
+    return n1, n2, n3, n4
+#int32_to_ip(2154959208)
+
+
+def binario(num):
+    valorBinario = ""
+    while num >1:
+        zeroOuUm = num % 2
+        valorBinario = str(zeroOuUm) + valorBinario
+        num = num // 2
+    print(valorBinario)
+binario(2)
