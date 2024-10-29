794c5bc168db6ef562b89c11164b2922162c5a8c
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-10-29 13:14:50 -0300

    solução

--- Exercícios/#022 - Land perimeter/solution.py
+++ Exercícios/#022 - Land perimeter/solution.py
@@ -21,4 +21,73 @@
 # should return: "Total land perimeter: 18"
 
 def land_perimeter(arr):
-    return  # perimeter of all the islands
+    terTotal = perTotal = 0
+    for posLinha, linha in enumerate(arr):
+        for posBloco, bloco in enumerate(linha):
+            if bloco == "X":
+                terTotal += 1
+                
+                # Se não for o da direita e a direita tiver preenchido:
+                if posBloco < (len(linha)-1): 
+                    if linha[posBloco+1] == "X":
+                        perTotal -= 1
+                        print(f"Menos 1 a Direita de X = {posLinha+1} Y = {posBloco+1}")
+
+                # Se não for o primeiro e a esquerda tiver preenchido:
+                if posBloco > 0: 
+                    if linha[posBloco-1] == "X":
+                        perTotal -= 1
+                        print(f"Menos 1 a Esquerda de X = {posLinha+1} Y = {posBloco+1}")
+
+                # Se não for o primeiro e em cima tiver preenchido:
+                if posLinha > 0:
+                    if arr[posLinha-1][posBloco] == "X":
+                        perTotal -= 1                    
+                        print(f"Menos 1 Acima de X = {posLinha+1} Y = {posBloco+1}")
+
+                # Se não for a última linha e em baixo tiver preenchido:
+                if posLinha < (len(arr)-1): 
+                    if arr[posLinha+1][posBloco] == "X":
+                        perTotal -= 1 
+                        print(f"Menos 1 Abaixo de X = {posLinha+1} Y = {posBloco+1}")
+
+    print(f"Área Total: {terTotal}")
+    print(f"Perímetro Total: {terTotal*4}")
+    perTotal += terTotal * 4
+    print(f"Perímetro Final: {perTotal}")
+    return  perTotal
+
+
+land_perimeter(["OXOOOX", "OXOXOO", "XXOOOX", "OXXXOO", "OOXOOX", "OXOOOO", "OOXOOX", "OOXOOO", "OXOOOO", "OXOOXX"])
+
+#  Para o CW:
+
+def land_perimeter(arr):
+    terTotal = perTotal = 0
+    for posLinha, linha in enumerate(arr):
+        for posBloco, bloco in enumerate(linha):
+            if bloco == "X":
+                terTotal += 1
+                
+                # Se não for o da direita e a direita tiver preenchido:
+                if posBloco < (len(linha)-1): 
+                    if linha[posBloco+1] == "X":
+                        perTotal -= 1
+
+                # Se não for o primeiro e a esquerda tiver preenchido:
+                if posBloco > 0: 
+                    if linha[posBloco-1] == "X":
+                        perTotal -= 1
+
+                # Se não for o primeiro e em cima tiver preenchido:
+                if posLinha > 0:
+                    if arr[posLinha-1][posBloco] == "X":
+                        perTotal -= 1                    
+
+                # Se não for a última linha e em baixo tiver preenchido:
+                if posLinha < (len(arr)-1): 
+                    if arr[posLinha+1][posBloco] == "X":
+                        perTotal -= 1 
+
+    perTotal += terTotal * 4
+    return  f"Total land perimeter: {perTotal}"
