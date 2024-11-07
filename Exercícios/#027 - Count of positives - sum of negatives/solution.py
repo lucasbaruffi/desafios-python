c82e6cd3601058ad60eccf39f5be40fff10b1995
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-07 13:17:31 -0300

    base 028

--- Exercícios/#027 - Count of positives - sum of negatives/solution.py
+++ Exercícios/#027 - Count of positives - sum of negatives/solution.py
@@ -1,12 +1,4 @@
-# https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/python
+# https://www.codewars.com/kata/5977618080ef220766000022/train/python
 
-def count_positives_sum_negatives(arr):
-    contPos = sumNeg = 0
-    for num in arr:
-        if num > 0:
-            contPos += 1
-        elif num < 0:
-            sumNeg += num
-    return [contPos, sumNeg] if arr else []
-
-print(count_positives_sum_negatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]))
+def usdcny(usd):
+    return "1 Chinese Yuan"
