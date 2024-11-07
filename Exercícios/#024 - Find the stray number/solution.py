14ec25cdd1c3d2f6bedb47b831f6e304500fe7ce
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-07 10:49:10 -0300

    deu boa

--- Exercícios/#024 - Find the stray number/solution.py
+++ Exercícios/#024 - Find the stray number/solution.py
@@ -1,4 +1,9 @@
 # https://www.codewars.com/kata/57f609022f4d534f05000024/train/python
 
 def stray(arr):
-    pass
+    for num in arr:
+        cont = arr.count(num)
+        if cont == 1:
+            return num
+
+print(stray([2, 3, 2, 2, 2]))
