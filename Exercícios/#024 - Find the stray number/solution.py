0c11cc112540d6bd1364e1e50028341c7d0cbac6
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-07 11:13:50 -0300

    base 024

--- Exercícios/#024 - Find the stray number/solution.py
+++ Exercícios/#024 - Find the stray number/solution.py
@@ -1,9 +1,4 @@
-# https://www.codewars.com/kata/57f609022f4d534f05000024/train/python
+# https://www.codewars.com/kata/5518a860a73e708c0a000027/train/python
 
-def stray(arr):
-    for num in arr:
-        cont = arr.count(num)
-        if cont == 1:
-            return num
-
-print(stray([2, 3, 2, 2, 2]))
+def last_digit(lst):
+    pass
