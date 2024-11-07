8fef09a2c89b9a78fda1269901268250ee223bc5
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-07 13:21:46 -0300

    isso

--- Exercícios/#027 - Count of positives - sum of negatives/solution.py
+++ Exercícios/#027 - Count of positives - sum of negatives/solution.py
@@ -1,4 +1,6 @@
 # https://www.codewars.com/kata/5977618080ef220766000022/train/python
 
 def usdcny(usd):
-    return "1 Chinese Yuan"
+    return f'{(usd * 6.75):.2f} Chinese Yuan'
+
+print(usdcny(465))
