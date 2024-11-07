215f609044141470af3aea6d6533ba58d723e64a
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-07 13:08:08 -0300

    deu boa

--- Exercícios/#026 - V A P O R C O D E/solution.py
+++ Exercícios/#026 - V A P O R C O D E/solution.py
@@ -1,4 +1,18 @@
 # https://www.codewars.com/kata/5966eeb31b229e44eb00007a/train/python
 
+
+#V1
 def vaporcode(s):
-    pass
+    s = s.upper()
+    s = s.replace(" ","")
+    s = list(s)
+    s = "  ".join(s)
+    return s
+
+# V2
+
+def vaporcode(s):
+    return "  ".join(list(s.upper().replace(" ","")))
+
+
+print(vaporcode("Lets go to the movies"))
