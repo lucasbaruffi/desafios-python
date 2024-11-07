2796865d072aad5944de9aba713b260e1ce12534
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-07 11:37:46 -0300

    não foi

--- Exercícios/#024 - Find the stray number/solution.py
+++ Exercícios/#024 - Find the stray number/solution.py
@@ -1,4 +1,19 @@
 # https://www.codewars.com/kata/5518a860a73e708c0a000027/train/python
 
 def last_digit(lst):
-    pass
+    lst.reverse()
+    totSum = 0
+    for contNum, num in enumerate(lst):
+        if contNum == 0:
+            totSum = pow(lst[contNum+1], num)
+        elif contNum == len(lst)-1:
+            pass
+        else:
+            totSum = pow(lst[contNum+1], totSum)
+        
+    totSum = str(totSum)
+    ultimo = totSum[-1]
+    return(ultimo)
+    
+
+last_digit([3, 4, 2])
