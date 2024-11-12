53dd428ace72ed96deac711ddb406f14c6a6dae9
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-12 15:03:55 -0300

    s1

--- Exercícios/#027 - Count of positives - sum of negatives/solution.py
+++ Exercícios/#027 - Count of positives - sum of negatives/solution.py
@@ -1,4 +1,14 @@
 # https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/python
 
 def move_zeros(lst):
+    zeros = 0
+    while 0 in lst:
+        indice = lst.index(0)
+        zeros += 1
+        lst.pop(indice)
+        print(indice)
+    for c in range(0,zeros):
+        lst.append(0)
     return lst
+
+print(move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
