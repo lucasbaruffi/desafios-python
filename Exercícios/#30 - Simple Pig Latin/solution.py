b2d0e96f1160594caee284ba494a5c09c97aa058
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-12 16:35:08 -0300

    eh, foi

--- Exercícios/#30 - Simple Pig Latin/solution.py
+++ Exercícios/#30 - Simple Pig Latin/solution.py
@@ -1,5 +1,27 @@
 # https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/python
 
+
+def replace_str_index(text,index=0,replacement=''):
+    return f'{text[:index]}{replacement}{text[index+1:]}'
+
+
 def pig_it(text):
-    #your code here
-    pass
+    textList = text.split()
+    newList = []
+    for palavra in textList:
+
+        if palavra.isalnum():
+
+            primeiraLetra = palavra[0]
+            palavra = replace_str_index(palavra)
+            palavra = palavra + primeiraLetra
+
+            palavra = palavra + "ay"
+
+        newList.append(palavra)
+
+    newList = " ".join(newList)
+    return newList
+
+
+print(pig_it('Panem et circenses'))
