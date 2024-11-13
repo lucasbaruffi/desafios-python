813bef0181951d9542f751c609b9e7929f602138
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-13 17:09:46 -0300

    não consegui acabar

--- Exercícios/#31 - RGB To Hex Conversion/solution.py
+++ Exercícios/#31 - RGB To Hex Conversion/solution.py
@@ -3,3 +3,21 @@
 def rgb(r, g, b):
     # your code here :)
     pass
+
+
+print(rgb(0, 0, 0), "000000", "testing zero values")
+print(rgb(1, 2, 3), "010203", "testing near zero values")
+print(rgb(255, 255, 255), "FFFFFF", "testing max values")
+print(rgb(254, 253, 252), "FEFDFC", "testing near max values")
+print(rgb(-20, 275, 125), "00FF7D", "testing out of range values")
+
+def toHex(n):
+    while True:
+        if n < 16:
+            break
+            
+        
+    pass
+
+
+print(toHex(52))
