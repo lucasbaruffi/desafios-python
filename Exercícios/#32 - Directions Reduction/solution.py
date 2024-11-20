6695339d71488a03b4e646777ae5140f73ddaf32
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-19 21:08:14 -0300

    deu boa

--- Exercícios/#32 - Directions Reduction/solution.py
+++ Exercícios/#32 - Directions Reduction/solution.py
@@ -1,4 +1,19 @@
 # https://www.codewars.com/kata/550f22f4d758534c1100025a/train/python
 
 def dir_reduc(arr):
-    pass
+    palavras = " ".join(arr)
+    while "EAST WEST" in palavras or "NORTH SOUTH" in palavras or "WEST EAST" in palavras or "SOUTH NORTH" in palavras:
+        palavras = " ".join(palavras.replace("EAST WEST","").replace("NORTH SOUTH","").replace("WEST EAST","").replace("SOUTH NORTH","").split())
+    return palavras.split()
+
+
+
+
+
+a = ['EAST', 'SOUTH', 'NORTH', 'NORTH', 'SOUTH', 'EAST', 'EAST', 'SOUTH', 'WEST', 'EAST', 'WEST', 'EAST', 'EAST', 'SOUTH', 'SOUTH', 'SOUTH']
+print(dir_reduc(a), ['WEST'])
+    
+
+# ['EAST', 'EAST', 'EAST', 'SOUTH', 'WEST', 'EAST', 'EAST', 'SOUTH', 'SOUTH', 'SOUTH'] should equal ['EAST', 'EAST', 'EAST', 'SOUTH', 'EAST', 'SOUTH', 'SOUTH', 'SOUTH']
+
+# ['EAST', 'EAST', 'EAST', 'SOUTH', 'EAST', 'SOUTH', 'SOUTH', 'SOUTH']
