55aaefcd33f7494077a8e684c4dd8d1f868e7055
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-22 13:55:36 -0300

    solução 034

--- Exercícios/#034 - Rot13/solution.py
+++ Exercícios/#034 - Rot13/solution.py
@@ -1,4 +1,16 @@
 # https://www.codewars.com/kata/530e15517bc88ac656000716/train/python
 
+alfabeto = [ "a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J", 
+            "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", 
+            "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z" ]
+
 def rot13(message): 
-    pass
+    palavra = ""
+    for letra in message:
+        if letra in alfabeto:
+            posFinal = alfabeto.index(letra) + 26
+            if posFinal >= 52:
+                posFinal -= 52
+            letra = alfabeto[posFinal]
+        palavra = palavra+letra
+    return palavra
