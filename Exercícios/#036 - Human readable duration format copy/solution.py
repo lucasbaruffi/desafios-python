3e65049a797a1566b75d3d37e03d6c2f4ea23914
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-26 20:13:56 -0300

    solução 037

--- Exercícios/#036 - Human readable duration format copy/solution.py
+++
@@ -1,5 +0,0 @@
-# https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/python
-
-def likes(names):
-    # your code here
-    pass
---
+++ Exercícios/#037 - Who likes it/solution.py
@@ -0,0 +1,21 @@
+# https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/python
+
+def likes(names):
+
+    if len(names) == 0:
+        return "no one like this"
+    elif len(names) == 1:
+        return f"{names[0]} likes this"
+    elif len(names) == 2:
+        return f"{names[0]} and {names[1]} likes this"
+    elif len(names) == 3:
+        return f"{names[0]}, {names[1]} and {names[2]} like this"
+    else:
+        return f"{names[0]}, {names[1]} and {len(names)-2} others like this"
+
+
+print(likes([]), 'no one likes this')
+print(likes(['Peter']), 'Peter likes this')
+print(likes(['Jacob', 'Alex']), 'Jacob and Alex like this')
+print(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this')
+print(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this')
