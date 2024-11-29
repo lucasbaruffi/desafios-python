4250b8ded13f524c378e25b5a3ab03873f51b249
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-29 11:57:33 -0300

    em duas linhas

--- Exercícios/#039 - Vowel Count/solution.py
+++ Exercícios/#039 - Vowel Count/solution.py
@@ -1,8 +1,4 @@
 # https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/python
 
-def get_count(sentence):
-    cont = 0
-    for letter in sentence:
-        if letter in "aeiou":
-            cont += 1
-    return cont
+def get_count(sentence): 
+    return (sentence.count("a")+sentence.count("e")+sentence.count("i")+sentence.count("o")+sentence.count("u"))
