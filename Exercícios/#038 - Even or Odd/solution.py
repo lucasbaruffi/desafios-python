d9e37fea27447cfc495af5a5f128847780c9e590
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-29 11:48:42 -0300

    em uma linha ta

--- Exercícios/#038 - Even or Odd/solution.py
+++ Exercícios/#038 - Even or Odd/solution.py
@@ -1,7 +1,7 @@
 # https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/python
 
-def even_or_odd(number):
-    return "Even" if number % 2 == 0 else "Odd"
+def even_or_odd(number): return "Even" if number % 2 == 0 else "Odd"
+    
 
 
 
