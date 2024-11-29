6d3cb8222db693ae3689215265a9888dbd9a4796
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-11-29 11:54:37 -0300

    lógica funcional

--- Exercícios/#039 - Vowel Count/solution.py
+++ Exercícios/#039 - Vowel Count/solution.py
@@ -1,4 +1,8 @@
 # https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/python
 
 def get_count(sentence):
-    pass
+    cont = 0
+    for letter in sentence:
+        if letter in "aeiou":
+            cont += 1
+    return cont
