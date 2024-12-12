eca8cd78d9ed8521866979957f1f2794c6032023
Lucas Baruffi <lucasbaruffi6@gmail.com>
2024-12-12 14:30:37 -0300

    pronto

--- Exercícios/#45 - internet check/solution.py
+++ Exercícios/#45 - internet check/solution.py
@@ -1,3 +1,20 @@
 # apenas para testar a internet
 
 # pip install speedtest-cli
+
+import speedtest as st
+
+def Speed_Test():
+    test = st.Speedtest()
+
+    down_speed = test.download()
+    down_speed = round(down_speed / 10**6, 2)
+    print(f"Download Speed in Mbps: {down_speed}")
+
+    up_speed = test.upload()
+    up_speed = round(up_speed / 10**6, 2)
+    print(f"Upload Speed in Mbps: {up_speed}")
+
+    ping = test.results.ping
+    print(f"Ping: {ping}")
+Speed_Test()
