2fae40f2b6d7a055b5f222321d8e4a6eb1913433
Lucas Baruffi <lucasbaruffi6@gmail.com>
2025-02-11 09:41:28 -0300

    Update solution.py

--- Exercícios/#046 - Validar CPF/solution.py
+++ Exercícios/#046 - Validar CPF/solution.py
@@ -7,7 +7,6 @@ cpf_verificado = ""
 for n in range(0,9):
     cpf_verificado += cpf[n]
 
-
 for n in range(0,2):
     # Soma os digitos
     soma_digitos = 0
@@ -27,3 +26,81 @@ if cpf == cpf_verificado:
 else:
     print(f"""CPF Inválido!
 CPF correto: {cpf_verificado}""")
+
+
+{
+    "id_origem_cliente": 50,
+    "descricao": "01 - Call - Ativo"
+},
+{
+    "id_origem_cliente": 51,
+    "descricao": "02 - Call - Receptivo"
+},
+{
+    "id_origem_cliente": 41,
+    "descricao": "03 - PAP - Próprio"
+},
+{
+    "id_origem_cliente": 87,
+    "descricao": "04 - Loja - Ativo"
+},
+{
+    "id_origem_cliente": 86,
+    "descricao": "05 - Loja - Online"
+},
+{
+    "id_origem_cliente": 18,
+    "descricao": "06 - Loja - Receptivo"
+},
+{
+    "id_origem_cliente": 25,
+    "descricao": "07 - Indicação"
+},
+{
+    "id_origem_cliente": 49,
+    "descricao": "10 - Call - Online"
+},
+{
+    "id_origem_cliente": 56,
+    "descricao": "11 - Call - Facebook/Instagram"
+},
+{
+    "id_origem_cliente": 57,
+    "descricao": "12 - Call - Google"
+},
+{
+    "id_origem_cliente": 94,
+    "descricao": "13 - PAP - Terceirizado"
+},
+{
+    "id_origem_cliente": 103,
+    "descricao": "14 - Ponto de Revenda"
+},
+{
+    "id_origem_cliente": 108,
+    "descricao": "16 - Call - TikTok"
+},
+{
+    "id_origem_cliente": 52,
+    "descricao": "20 - ActionCall"
+},
+{
+    "id_origem_cliente": 93,
+    "descricao": "21 - UPcall"
+},
+{
+    "id_origem_cliente": 60,
+    "descricao": "30 - B2B"
+},
+{
+    "id_origem_cliente": 55,
+    "descricao": "31 - Licitação / Pregão - Público"
+},
+{
+    "id_origem_cliente": 106,
+    "descricao": "98 - Funcionário Brayo"
+},
+{
+    "id_origem_cliente": 54,
+    "descricao": "99 - Cortesia"
+}
